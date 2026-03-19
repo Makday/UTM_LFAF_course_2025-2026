e2b3db4f9ef5f71aee7f9af27223514435aefe09
Makday <58140558+Makday@users.noreply.github.com>
2026-03-19 03:17:00 +0200

    add lexer with basic functionalities

---
+++ Lab3/src/main/java/lfaf/university/labs2026/Lexer.java
@@ -0,0 +1,64 @@
+package lfaf.university.labs2026;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Lexer {
+    private final String input;
+    private int pos;
+
+    private static final Set<String> KEYWORDS = Set.of(
+            "SELECT", "FROM", "WHERE", "AND", "OR", "NOT",
+            "INSERT", "INTO", "VALUES",
+            "UPDATE", "SET",
+            "DELETE",
+            "ORDER", "BY", "ASC", "DESC",
+            "LIMIT", "TRUE", "FALSE"
+    );
+
+    public Lexer(String input) {
+        this.input = input;
+        this.pos = 0;
+    }
+
+    public List<Token> tokenize() {
+        List<Token> tokens = new ArrayList<>();
+
+        while (pos < input.length()) {
+            skipWhitespace();
+            if (pos >= input.length()) break;
+
+            char c = current();
+
+            if (Character.isLetter(c) || c == '_') {
+                tokens.add(readWord());
+            } else if (Character.isDigit(c)) {
+                tokens.add(readNumber());
+            } else if (c == '\'') {
+                tokens.add(readString());
+            } else {
+                tokens.add(readSymbol());
+            }
+        }
+
+        tokens.add(new Token(TokenType.EOF));
+        return tokens;
+    }
+
+
+    private void skipWhitespace() {
+        while (pos < input.length() && Character.isWhitespace(current())) {
+            pos++;
+        }
+    }
+
+    private char current() {
+        return input.charAt(pos);
+    }
+
+    private char peek() {
+        if(pos + 1 < input.length()) return input.charAt(pos+1);
+        else return current();
+    }
+}
