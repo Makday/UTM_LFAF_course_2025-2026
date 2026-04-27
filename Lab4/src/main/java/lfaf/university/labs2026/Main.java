fe5c3be7aa04ecaf9f60d0bd0f2f3912baf926b9
Makday <58140558+Makday@users.noreply.github.com>
2026-04-27 12:24:11 +0300

    add examples

---
+++ Lab4/src/main/java/lfaf/university/labs2026/Main.java
@@ -0,0 +1,10 @@
+package lfaf.university.labs2026;
+
+public class Main {
+    static void main() {
+        var generator = new RegexGenerator();
+        System.out.println(generator.generate("M?N{2}(O|P){3}Q*R+"));
+        System.out.println(generator.generate("(X|Y|Z){3}8+(9|0)"));
+        System.out.println(generator.generate("(H|i)(J|K)L*N?"));
+    }
+}
